ec2f7c25ca7b370013845cfd34089d0ebbe0f661
mariothedog <mariothedogmc@gmail.com>
2020-05-09 17:58:11 +0100

    Removed other tile sheets + Fixed tile map + Fixed controls

--- UI/HUD/HUD.gd
+++ UI/HUD/HUD.gd
@@ -1,4 +1,5 @@
 extends CanvasLayer
 
 func open_console():
-	get_tree().change_scene("res://UI/Hacking/Hacking GUI.tscn")
+	if get_tree().change_scene("res://UI/Hacking/Hacking GUI.tscn") != OK:
+		print_debug("An error occured while attempting to open the console.")
