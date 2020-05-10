7b2c779d480fe63df1bf3428c657ac32fb6fdf54
mariothedog <mariothedogmc@gmail.com>
2020-05-10 21:10:22 +0100

    GUI stuff

--- UI/Hacking/Hacking GUI.gd
+++ UI/Hacking/Hacking GUI.gd
@@ -7,16 +7,16 @@ var success_text = "The \"%s\" command has been used successfully."
 var invalid_syntax_text = "Please use the correct syntax."
 var not_recognised_text = "\'%s\' is not recognised as an internal or external command,\noperable program or batch file."
 
-var usable_commands = {
-	# command : min parameter size
-	"set" : 3,
-	"get" : 2,
-	"exit" : 1,
-	"restart" : 1,
-	"fly" : 2,
-	"help" : 1,
-	"variables": 1
-}
+#var usable_commands = {
+#	# command : min parameter size
+#	"set" : 3,
+#	"get" : 2,
+#	"exit" : 1,
+#	"restart" : 1,
+#	"fly" : 2,
+#	"help" : 1,
+#	"variables": 1
+#}
 
 func _on_Console_command_entered(command):
 	parse_command(command)
@@ -26,63 +26,110 @@ func parse_command(text):
 	var separated_command = text.split(" ")
 	
 	# Lower case to ensure that even if the player adds an extra capital somewhere that it will still work fine.
-	var command = separated_command[0].to_lower()
+	var command_str = separated_command[0].to_lower()
+	var command = global.command.get_command(command_str)
 	
-	# Some commands need more parameters than others. E.g. The set command needs the variable changed and the new value while exit doesn't need anything else.
-	var min_size = usable_commands.get(command)
-	if not min_size:
-		console.output_error(not_recognised_text % command)
+	# If the command doesn't exist.
+	if not command:
+		console.output_error(not_recognised_text % command_str)
 		return
 	
+	# Some commands need more parameters than others. E.g. The set command needs the variable changed and the new value while exit doesn't need anything else.
+	#var min_size = usable_commands.get(command)
+#	if not min_size:
+#		console.output_error(not_recognised_text % command)
+#		return
+	
 	# If the user doesn't use enough parameters then output an error.
-	if separated_command.size() < min_size:
+#	if separated_command.size() < min_size:
+#		console.output_error(invalid_syntax_text)
+#		return
+	
+	if separated_command.size() < command.min_parameters:
 		console.output_error(invalid_syntax_text)
 		return
 	
 	# Different commands do different things.
-	match command:
+	match command.name:
 		"set":
-			var variable = separated_command[1].to_lower()
+#			var variable = separated_command[1].to_lower()
+#
+#			# Check if the variable actually exists.
+#			var value = global.get(variable)
+#			if not value:
+#				console.output_error(not_recognised_text % variable)
+#				return
+#
+#			# Change the variable's value to the new value that the player chose.
+#			var new_value = separated_command[2].to_lower()
+#			global.get(variable).value=new_value
+#
+#			console.output_text("[i]%s has been set to %s.[/i]" % [variable, new_value], false)
 			
-			# Check if the variable actually exists.
-			var value = global.get(variable)
-			if not value:
+			var variable_str = separated_command[1].to_lower()
+			var variable = global.gamevar.get_gamevar(variable_str)
+			if not variable:
 				console.output_error(not_recognised_text % variable)
 				return
 			
-			# Change the variable's value to the new value that the player chose.
 			var new_value = separated_command[2].to_lower()
-			global.get(variable).value=new_value
+			variable.value = new_value
 			
-			console.output_text("[i]%s has been set to %s.[/i]" % [variable, new_value], false)
+			console.output_text("[i]%s has been set to %s.[/i]" % [variable_str, new_value], false)
 		"get":
-			var variable = separated_command[1].to_lower()
+#			var variable = separated_command[1].to_lower()
+#
+#			# Check if the variable actually exists.
+#			var value = global.get(variable.value)
+#			if not value:
+#				console.output_error(not_recognised_text % variable)
+#				return
+#
+#			# If it does then output the variable's value.
+#			console.output_text(value, false)
 			
-			# Check if the variable actually exists.
-			var value = global.get(variable.value)
-			if not value:
+			var variable_str = separated_command[1].to_lower()
+			var variable = global.gamevar.get_gamevar(variable_str)
+			if not variable:
 				console.output_error(not_recognised_text % variable)
 				return
 			
-			# If it does then output the variable's value.
-			console.output_text(value, false)
+			console.output_text(variable.value, false)
 		"exit":
 			exit()
 		"restart":
 			exit()
 			restart()
 		"help":
-			console.output_text("set: to set variable values\nget: to get variable values\nexit: back to da game\nrestart: Everyone has a second chance", false)
+			#console.output_text("set: to set variable values\nget: to get variable values\nexit: back to da game\nrestart: Everyone has a second chance", false)
+			var output = "Commands:\n"
+			for command in global.commands_list:
+				output += str(command.name, " - ", command.description, "\n")
+			output = output.trim_suffix("\n")
+			console.output_text(output, false)
 		"variables":
-			var list=global.get_property_list()
-			var gamevars= []
-			console.output_text("list of variables",false)
-			for item in list:
-				if item["type"]==typeof(global.gamevar):
-					gamevars.push_back(item["name"])
-			for item in gamevars:
-				if global.get(item).writable:
-					console.output_text(item+": "+global.get(item).description,false)
+#			var properties = global.get_property_list()
+#			#var gamevars = []
+#			var output = "Variables:\n"
+#			#console.output_text("list of variables",false)
+#			for property in properties:
+#				#print(item.get_class())
+#				#if item["type"]==typeof(global.gamevar):
+#				#	gamevars.push_back(item["name"])
+#				var variable = global.get(property["name"])
+#				if variable and typeof(variable) == TYPE_OBJECT and variable.get_class() == "Reference" and not variable.command:
+#					output += str(property["name"], " - ", variable.description, "\n")
+#			#print(gamevars)
+#			#for item in gamevars:
+#			#	if global.get(item).writable:
+#			#		console.output_text(item+": "+global.get(item).description,false)
+			
+			var output = "Variables:\n"
+			for variable in global.gamevars_list:
+				output += "%s : %s - %s\n" % [variable.aliases[0], variable.type, variable.description]
+			
+			output = output.trim_suffix("\n")
+			console.output_text(output, false)
 
 func exit():
 	emit_signal("exit_pressed")
--- global.gd
+++ global.gd
@@ -1,32 +1,81 @@
 extends Node
 
-# All variables in here will be changeable in the console.
-class gamevar:
-	var value:String
-	var description:String
-	var writable: bool=true
+var commands_list = []
+var gamevars_list = []
+
+class command:
+	var name
+	var description
+	var min_parameters
 	
-func gamevar(var value:String="", var description:String="", var writable:bool=true):
-	var variable=gamevar.new()
-	variable.value=value
-	variable.description=description
-	variable.writable=writable
-	variable.writable=writable
-	return variable
+	func _init(_name = "", _description = "", _min_parameters = 0):
+		self.name = _name
+		self.description = _description
+		self.min_parameters = _min_parameters
+		global.commands_list.append(self)
+	
+	static func get_command(cmd_name):
+		for command in global.commands_list:
+			if command.name == cmd_name:
+				return command
 
-var gravity:gamevar
-var player_fly:gamevar
-var player_max_speed:gamevar
-var player_max_acc:gamevar
-var player_max_friction:gamevar
-var player_jump_speed:gamevar
-var player_health:gamevar
+class gamevar:
+	var aliases
+	var value#:String
+	var type
+	var description#:String
+	var writable#: bool=true # Already set to true by default in the init.
+	
+	func _init(_aliases = [], _value = "", _type = "String", _description = "", _writable = true):
+		self.aliases = _aliases
+		self.value = _value
+		self.type = _type
+		self.description = _description
+		self.writable = _writable
+		global.gamevars_list.append(self)
+	
+	static func get_gamevar(name):
+		for gamevar in global.gamevars_list:
+			for alias in gamevar.aliases:
+				if name == alias:
+					return gamevar
 
-func _init():
-	gravity= gamevar("500", "The value of the gravity")
-	player_fly= gamevar("false", "The player's ability to fly")
-	player_max_speed= gamevar("600", "The maximum speed at which the player can go")
-	player_max_acc= gamevar("70", "The maximum acceleration the player can be applying")
-	player_max_friction= gamevar("60","The maximum friction that can be applied to the player")
-	player_jump_speed=gamevar("200", "The speed (or force) applied to the player when jumping")
-	player_health=gamevar("100", "The health that the player has")
+#func gamevar(var value:String="", var description:String="", var writable:bool=true):
+#	var variable=gamevar.new()
+#	variable.value=value
+#	variable.description=description
+#	variable.writable=writable
+#	#variable.writable=writable # No need for two.
+#	return variable
+
+#var gravity : gamevar
+#var player_fly : gamevar
+#var player_max_speed : gamevar
+#var player_max_acc : gamevar
+#var player_max_friction : gamevar
+#var player_jump_speed : gamevar
+#var player_health : gamevar
+
+onready var help_command = command.new("help", "Display a list of commands", 1)
+onready var variables_command = command.new("variables", "Display a list of variables", 1)
+onready var set_command = command.new("set", "Set a specified variable to another value", 3)
+onready var get_command = command.new("get", "Return a specified variable", 2)
+onready var exit_command = command.new("exit", "Exit the hacking interface", 1)
+onready var restart_command = command.new("restart", "Restart the game", 1)
+
+onready var gravity = gamevar.new(["gravity"], "500", "Number", "The value of the gravity")
+onready var player_fly = gamevar.new(["player_fly"], "false", "True/False", "The player's ability to fly")
+onready var player_max_speed = gamevar.new(["player_max_speed", "player_speed"], "600", "Number", "The maximum speed at which the player can go")
+onready var player_max_acc = gamevar.new(["player_max_acc"], "70", "Number", "The maximum acceleration the player can be applying")
+onready var player_max_friction = gamevar.new(["player_max_friction"], "60", "Number", "The maximum friction that can be applied to the player")
+onready var player_jump_speed = gamevar.new(["player_jump_speed"], "200", "Number", "The speed (or force) applied to the player when jumping")
+onready var player_health = gamevar.new(["player_health"], "100", "Number", "The health that the player has")
+
+#func _init():
+#	gravity= gamevar("500", "The value of the gravity")
+#	player_fly= gamevar("false", "The player's ability to fly")
+#	player_max_speed= gamevar("600", "The maximum speed at which the player can go")
+#	player_max_acc= gamevar("70", "The maximum acceleration the player can be applying")
+#	player_max_friction= gamevar("60","The maximum friction that can be applied to the player")
+#	player_jump_speed=gamevar("200", "The speed (or force) applied to the player when jumping")
+#	player_health=gamevar("100", "The health that the player has")
