2e1118fa9ddcab63b58c87917ac39e6e82a29727
mariothedog <mariothedogmc@gmail.com>
2020-05-15 22:14:43 +0100

    Improved clouds.

--- Sky/Sky.gd
+++ Sky/Sky.gd
@@ -1,8 +1,11 @@
 extends ParallaxBackground
 
 onready var clouds = get_node("Clouds")
+onready var clouds2 = get_node("Clouds2")
 
-var cloud_velocity = Vector2(30, 0)
+var cloud_velocity = Vector2(10, 0)
+var cloud2_velocity = Vector2(30, 0)
 
 func _process(delta):
 	clouds.motion_offset = clouds.motion_offset + cloud_velocity * delta
+	clouds2.motion_offset = clouds2.motion_offset + cloud2_velocity * delta
